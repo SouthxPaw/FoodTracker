09614ab556b620f3b826c92f0b6777d759a62215
SouthxPaw <40015860+SouthxPaw@users.noreply.github.com>
2019-10-30 23:24:44 -0400

    Food Oasis Application UI
    A general idea for UI for the Food Oasis UI

---
+++ app/src/main/java/com/foodtracker/foodoasis/VendorList.java
@@ -0,0 +1,14 @@
+package com.foodtracker.foodoasis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VendorList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_list);
+    }
+}
